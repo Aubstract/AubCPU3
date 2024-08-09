95bd3140ce5a4f3f2445f37b63dd1a462ea97fee
Ben Fields <fieldsben5@gmail.com>
2024-08-09 16:31:17 -0700

    Added schematic generation and cli arguments to assembler

--- src/assembler/main.py
+++ src/assembler/main.py
@@ -1,4 +1,8 @@
 # this will be the entry point for the assembler
+from datetime import datetime
+import argparse
+import mcschematic as mcschem
+
 import dictionaries as dicts
 import link
 import preprocess as pre
@@ -24,24 +28,106 @@ def load_program(file_path: str) -> list[str]:
     return program
 
 
+def write_bin_file(path: str, program: list[int]) -> None:
+    with open(f"{path}.bin", "wb") as f:
+        for value in program:
+            # Convert integer to 2-byte bytearray with little-endian byte order
+            bytes_data = value.to_bytes(2, byteorder="little")
+            f.write(bytes_data)
+
+
+def generate_signal_strengths(lower_byte1, upper_byte1, lower_byte2, upper_byte2) -> list[int]:
+    bitmask = 0x80
+    return_list = []
+    for index in range(7, -1, -1):
+        ss = 0
+        if lower_byte1 & bitmask:
+            ss |= 1
+        if lower_byte2 & bitmask:
+            ss |= 2
+        if upper_byte1 & bitmask:
+            ss |= 4
+        if upper_byte2 & bitmask:
+            ss |= 8
+        return_list.append(ss)
+        bitmask >>= 1
+
+    return return_list
+
+
+def write_schem_file(path: str, file_name: str, program: list[int]) -> None:
+    schem = mcschem.MCSchematic()
+
+    # Offset of schem origin from player's feet
+    origin_x = 1
+    origin_y = -2
+    origin_z = 1
+
+    if len(program) % 2 != 0:
+        program.append(0)
+
+    for index, (instr1, instr2) in enumerate(zip(program[::2], program[1::2])):
+        # index starts at 0 and counts up by 1
+        coord_x = origin_x + ((index // 8) * 2) + ((index // 16) % 2)
+        coord_y = origin_y + (index // 16) % 2
+        coord_z = origin_z + (index % 8) * 2
+
+        lower_byte1 = instr1 & 0xFF
+        upper_byte1 = instr1 >> 8
+        lower_byte2 = instr2 & 0xFF
+        upper_byte2 = instr2 >> 8
+
+        signal_strengths = generate_signal_strengths(lower_byte1, upper_byte1, lower_byte2, upper_byte2)
+
+        for height, ss in enumerate(signal_strengths):
+            schem.setBlock((coord_x, coord_y - (2 * height), coord_z), mcschem.BlockDataDB.BARREL.fromSS(ss))
+
+    # Construct the sign's NBT data
+    now = datetime.now()
+    line1 = file_name
+    line2 = "Assembled:"
+    line3 = now.strftime("%Y-%m-%d")
+    line4 = f"""{now.strftime("%H:%M:%S")}"""
+    block_data = ("""minecraft:birch_sign[rotation=8,waterlogged=false]"""
+                  + """{GlowingText: 0b, Color: "black", Text4: '{"text":" """
+                  + line4 + """ "}', Text3: '{"text":" """
+                  + line3 + """ "}', Text2: '{"text":" """
+                  + line2 + """ "}', Text1: '{"text":" """
+                  + line1 + """ "}'}""")
+    schem.setBlock((0, 0, 0), block_data)
+
+    schem.save(path, file_name, mcschem.Version.JE_1_18_2)
+
+
 def main():
-    # Assemble program
+    msg = "Assemble a file"
+    parser = argparse.ArgumentParser(description=msg)
+    # parser.add_argument("-src", "--source", help="path to source file")
+    parser.add_argument("-bin", "--binary", action="store_true", help="output a binary file")
+    parser.add_argument("-schem", "--schematic", action="store_true", help="output a schematic file")
+    parser.add_argument("-odir", "--outputDirPath", help="Path to the output directory")
+    args = parser.parse_args()
+
+    #if args.source:
+    #    dicts.src_file_path = args.source
+    #else:
+
     dicts.src_file_path = get_file_path()
+
     program = load_program(dicts.src_file_path)
     program = link.link(program)
     program = pre.preprocess(program)
     program = assemble.assemble(program)
     
-    # Build output binary file path
+    # Build output file path
     file_name = dicts.src_file_path.split("\\")[-1]
     file_name = file_name[:file_name.index('.')]
-    
+
     # Write to binary file
-    with open(f"bin_files/{file_name}.bin", "wb") as f:
-        for value in program:
-            # Convert integer to 2-byte bytearray with little-endian byte order
-            bytes_data = value.to_bytes(2, byteorder="little")
-            f.write(bytes_data)
+    if args.binary:
+        write_bin_file(args.outputDirPath + file_name, program)
+    elif args.schematic:
+        write_schem_file(args.outputDirPath, file_name, program)
     
     # Post process
     post.postprocess(program)
